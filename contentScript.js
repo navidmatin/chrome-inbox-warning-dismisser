79d2fd513234ec2cbcf67e4ae5e55281a33d6296
Navid Matin <navidmatin@users.noreply.github.com>
2019-03-25 18:51:34 -0400

    Added interval to timeout call

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,12 @@
 window.onload = function() {
-  document.getElementsByClassName("AK eX B6")[0].click();
+  var tryToDismissInterval = setInterval(TryToDismiss, 1 * 1000);
+  setTimeout(function() {clearInterval(tryToDismissInterval)}, 10 * 1000);
 };
+
+function TryToDismiss(){
+  var dom = document.getElementsByClassName("AK eX B6");
+  if(dom.length > 0)
+  {
+    dom[0].click();
+  }
+}
